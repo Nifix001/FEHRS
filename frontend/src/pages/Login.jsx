eac8946acc757e25d4e9b57995b99a652ca74e3c
MrLad01 <ladayodeji@gmail.com>
2023-08-02 17:35:08 +0100

    update

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import logo from '../assets/Frames.png'
+import { Form } from 'react-router-dom'
 
 const Login = () => {
   return (
     <React.Fragment>
-      <div>Hello World!</div>
+      <section className='grid'>
+        <header><img src={logo} alt="" /></header>
+        <Form>
+            <header>FUTA Electronic Health Record System</header>
+            <input type="text" name='uniqueId' placeholder='Unique ID' />
+            <input type="text" name='password' placeholder='Password' />
+            <button type='submit'>Login</button>
+        </Form>
+      </section>
     </React.Fragment>
   )
 }
