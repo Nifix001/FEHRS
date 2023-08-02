397085b1a1f70a38dc333193d197281900de87e5
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-08-02 17:07:49 +0100

    update

--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -5,17 +5,19 @@ import logo from "../assets/Frames.png"
 const Welcome = () => {
   return (
     <React.Fragment>
+      <div className="bg-background">
       <header>
         <img src={logo} alt="" />
       </header>
       <section>
-      <h2>Welcome to <br /> <span>FEHRS</span></h2>
+      <h2>Welcome to <br /><span>FEHRS</span></h2>
       <h4>FUTA Electronic Health Record System</h4>
       <div>
         <Link>Login</Link>
         <Link>Sign up</Link>
       </div>
       </section>
+      </div>
     </React.Fragment>
   )
 }
