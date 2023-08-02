2630760d68704b49e2ae5031a582b411c672e401
MrLad01 <ladayodeji@gmail.com>
2023-08-02 16:31:09 +0100

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,31 @@
+import { createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom"
 
 function App() {
 
+
+  
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+                  <Route index element={<w />} />
+                  <Route path="log-in" element={<LogIn />} />
+                   <Route path="/" element={<LandingLayout />}>
+                      <Route index element={<HomeLayout />} />
+                      <Route path="services" element={<Services />}>
+                         <Route path="others" element={<Other />}>
+                          <Route path="footer" element={<Footer />} />
+                        </Route>
+                      </Route>
+                      <Route path="help" element={<Help />} />
+                    </Route>  
+                </Route>
+             
+           )
+         )
+
   return (
     <>
+      <RouterProvider router={router} />
     </>
   )
 }
