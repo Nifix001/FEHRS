46a581a348988cccb6315b3ca2c78f62d5610e3e
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-08-03 19:02:48 +0100

    update

--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -13,7 +13,7 @@ const Welcome = () => {
       <h2 className="text-center mt-24"><span className="font-welcome text-8xl text-white font-bold tracking-wide">Welcome to</span> <br /><span className="font-span text-8xl text-gray-300 opacity-70  font-semibold tracking-wide">FEHRS</span></h2>
       <h4 className="text-center font-welcome font-normal text-white text-3xl mt-6 leading-8">FUTA Electronic Health Record System</h4>
       <div className="flex justify-center items-center mt-20">
-        <Link className="font-span bg-transparent text-white text-l font-bold text-center mr-4 px-14 py-2 border border-solid border-gray-300 rounded-lg hover:bg-primary hover:border-primary">Login</Link>
+        <Link to="/login" className="font-span bg-transparent text-white text-l font-bold text-center mr-4 px-14 py-2 border border-solid border-gray-300 rounded-lg hover:bg-primary hover:border-primary">Login</Link>
         <Link className="font-span bg-transparent text-white text-l font-bold text-center px-14 py-2 border border-solid border-gray-300 rounded-lg hover:bg-primary hover:border-primary">Sign up</Link>
       </div>
       </section>
