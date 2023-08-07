079bcb24539cc0b07ba823e6faaeb6d7e88e0b9f
MrLad01 <ladayodeji@gmail.com>
2023-08-07 18:40:48 +0100

    update

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -3,7 +3,41 @@ import React from 'react'
 const SideBar = () => {
   return (
     <React.Fragment>
-      <div className='w-60 h-224 bg-white absolute left-0'></div>
+      <div className='w-60 h-224 bg-white absolute left-0 grid'>
+        <header>Menu</header>
+        <div className='flex'>
+            <img src="" alt="" />
+            <h3>Dashboard</h3>
+        </div>
+        <div className='flex'>
+            <img src="" alt="" />
+            <h3>Patients</h3>
+        </div>
+        <div className='flex'>
+            <img src="" alt="" />
+            <h3>Doctor</h3>
+        </div>
+        <div className='flex'>
+            <img src="" alt="" />
+            <h3>Pharmacy</h3>
+        </div>
+        <div className='flex'>
+            <img src="" alt="" />
+            <h3>Reports</h3>
+        </div>
+        <div className='flex'>
+            <img src="" alt="" />
+            <h3>Settings</h3>
+        </div>
+        <div className='grid absolute bottom-0'>
+            <hr />
+            <h3>General</h3>
+            <div>
+                <img src="" alt="" />
+                <h3>Support</h3>
+            </div>
+        </div>
+      </div>
     </React.Fragment>
   )
 }
