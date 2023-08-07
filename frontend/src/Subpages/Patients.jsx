664d3946888c285eb887e9280856e2f4e9041598
MrLad01 <ladayodeji@gmail.com>
2023-08-07 21:30:34 +0100

    update

--- frontend/src/Subpages/Patients.jsx
+++ frontend/src/Subpages/Patients.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Patients = () => {
   return (
     <React.Fragment>
-     <div className='w-281 h-142.5 bg-white relative top-20 left-60'>
+     <div className='w-281 h-142.5 bg-gray-750 relative top-20 left-60 -ml-0.5'>
         Hello
      </div>
     </React.Fragment>
