099ab8b3f93416519e0d5d822f3caa698463e374
MrLad01 <ladayodeji@gmail.com>
2023-08-07 19:57:23 +0100

    update

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -14,27 +14,27 @@ const SideBar = () => {
       <div className='w-60 h-143 bg-white absolute left-0 top-20 pl-4 sidebar'>
         <header className='mt-7 mb-3'>Menu</header>
         <div className="grid">
-            <NavLink className='flex hover:bg-primary hover:text-white p-1.5 -ml-1.5'>
+            <NavLink className='flex hover:bg-primary hover:text-white p-1.5 -ml-1.5' to="">
                 <img className='w-5 h-5 mr-2' src={dashIcon} alt="dashboard icon" />
                 <h3 className='-mt-0.5'>Dashboard</h3>
             </NavLink>
-            <NavLink className='flex hover:bg-primary hover:text-white p-1.5 -ml-1.5'>
+            <NavLink className='flex hover:bg-primary hover:text-white p-1.5 -ml-1.5' to="">
                 <img className='w-5 h-5 mr-2' src={PatientIcon} alt="patient icon" />
                 <h3 className='-mt-0.5'>Patients</h3>
             </NavLink>
-            <NavLink className='flex hover:bg-primary hover:text-white p-1.5 -ml-1.5'>
+            <NavLink className='flex hover:bg-primary hover:text-white p-1.5 -ml-1.5' to="">
                 <img className='w-5 h-5 mr-2' src={doctorIcon}alt="doctor icon" />
                 <h3 className='-mt-0.5'>Doctor</h3>
             </NavLink>
-            <NavLink className='flex hover:bg-primary hover:text-white p-1.5 -ml-1.5'>
+            <NavLink className='flex hover:bg-primary hover:text-white p-1.5 -ml-1.5' to="">
                 <img className='w-5 h-5 mr-2' src={PharmacyIcon} alt="pharmacy icon" />
                 <h3 className='-mt-0.5'>Pharmacy</h3>
             </NavLink>
-            <NavLink className='flex hover:bg-primary hover:text-white p-1.5 -ml-1.5'>
+            <NavLink className='flex hover:bg-primary hover:text-white p-1.5 -ml-1.5' to="">
                 <img className='w-5 h-5 mr-2' src={ReportsIcon} alt="reports icon" />
                 <h3 className='-mt-0.5'>Reports</h3>
             </NavLink>
-            <NavLink className='flex hover:bg-primary hover:text-white p-1.5 -ml-1.5'>
+            <NavLink className='flex hover:bg-primary hover:text-white p-1.5 -ml-1.5' to="">
                 <img className='w-5 h-5 mr-2' src={SettingsIcon} alt="settings icon" />
                 <h3 className='-mt-0.5'>Settings</h3>
             </NavLink>
