ba73ed95232cd1067c66424efa9ad8dd0c9f2d33
MrLad01 <ladayodeji@gmail.com>
2023-08-07 21:05:39 +0100

    update

--- frontend/src/Subpages/Patients.jsx
+++ frontend/src/Subpages/Patients.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Patients = () => {
   return (
-    <div className='w-10 h-142 bg-gray-750 absolute right-0 top-20'>
+    <div className='w-80 h-142 bg-gray-750 relative top-20'>
       Hello
     </div>
   )
