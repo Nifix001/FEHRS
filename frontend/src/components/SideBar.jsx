f515c1817fdf742471489e2b2b042d2d49a75043
MrLad01 <ladayodeji@gmail.com>
2023-08-07 19:41:53 +0100

    update

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -5,7 +5,7 @@ import doctorIcon from '../assets/sidebar/hospital.png';
 import PharmacyIcon from '../assets/sidebar/building-3.png';
 import ReportsIcon from '../assets/sidebar/chart.png';
 import SettingsIcon from '../assets/sidebar/ant-design_setting-outlined.png';
-// import SettingsIcon from '../assets/sidebar/ic_outline-contact-support.png'
+import SupportIcon from '../assets/sidebar/ic_outline-contact-support.png'
 
 const SideBar = () => {
   return (
@@ -15,35 +15,35 @@ const SideBar = () => {
         <div className="grid gap-3">
             <div className='flex'>
                 <img className='w-5 h-5 mr-2' src={dashIcon} alt="dashboard icon" />
-                <h3>Dashboard</h3>
+                <h3 className='-mt-0.5'>Dashboard</h3>
             </div>
             <div className='flex'>
                 <img className='w-5 h-5 mr-2' src={PatientIcon} alt="patient icon" />
-                <h3>Patients</h3>
+                <h3 className='-mt-0.5'>Patients</h3>
             </div>
             <div className='flex'>
                 <img className='w-5 h-5 mr-2' src={doctorIcon}alt="doctor icon" />
-                <h3>Doctor</h3>
+                <h3 className='-mt-0.5'>Doctor</h3>
             </div>
             <div className='flex'>
                 <img className='w-5 h-5 mr-2' src={PharmacyIcon} alt="pharmacy icon" />
-                <h3>Pharmacy</h3>
+                <h3 className='-mt-0.5'>Pharmacy</h3>
             </div>
             <div className='flex'>
                 <img className='w-5 h-5 mr-2' src={ReportsIcon} alt="reports icon" />
-                <h3>Reports</h3>
+                <h3 className='-mt-0.5'>Reports</h3>
             </div>
             <div className='flex'>
                 <img className='w-5 h-5 mr-2' src={SettingsIcon} alt="settings icon" />
-                <h3>Settings</h3>
+                <h3 className='-mt-0.5'>Settings</h3>
             </div>
         </div>
-        <div className='grid absolute bottom-0'>
+        <div className='grid absolute bottom-20'>
             <hr className='w-60' />
-            <h3>General</h3>
-            <div>
-                <img className='w-5 h-5 mr-2' src="" alt="" />
-                <h3>Support</h3>
+            <h3 className='my-4'>General</h3>
+            <div className='flex'>
+                <img className='w-5 h-5 mr-2' src={SupportIcon} alt="" />
+                <h3 className='-mt-0.5'>Support</h3>
             </div>
         </div>
       </div>
