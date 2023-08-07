df7accd73efe5ed68e4cf68008d5f4f16f71dafa
MrLad01 <ladayodeji@gmail.com>
2023-08-07 20:55:03 +0100

    update

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -18,7 +18,7 @@ const SideBar = () => {
                 <img className='w-5 h-5 mr-2' src={dashIcon} alt="dashboard icon" />
                 <h3 className='-mt-0.5'>Dashboard</h3>
             </NavLink>
-            <NavLink className='flex hover:bg-primary hover:text-white p-1.5 -ml-1.5' to="/patient">
+            <NavLink className='flex hover:bg-primary hover:text-white p-1.5 -ml-1.5' to="patient">
                 <img className='w-5 h-5 mr-2' src={PatientIcon} alt="patient icon" />
                 <h3 className='-mt-0.5'>Patients</h3>
             </NavLink>
