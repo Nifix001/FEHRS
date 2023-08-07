226d10c3048fdf2345b13beae937c2deafb2ba82
MrLad01 <ladayodeji@gmail.com>
2023-08-07 19:27:29 +0100

    update

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -10,37 +10,39 @@ import SettingsIcon from '../assets/sidebar/ant-design_setting-outlined.png';
 const SideBar = () => {
   return (
     <React.Fragment>
-      <div className='w-60 h-143 bg-white absolute left-0 grid top-20 '>
+      <div className='w-60 h-143 bg-white absolute left-0 top-20 '>
         <header>Menu</header>
-        <div className='flex'>
-            <img src={dashIcon} alt="dashboard icon" />
-            <h3>Dashboard</h3>
-        </div>
-        <div className='flex'>
-            <img src={PatientIcon} alt="patient icon" />
-            <h3>Patients</h3>
-        </div>
-        <div className='flex'>
-            <img src={doctorIcon}alt="doctor icon" />
-            <h3>Doctor</h3>
-        </div>
-        <div className='flex'>
-            <img src={PharmacyIcon} alt="pharmacy icon" />
-            <h3>Pharmacy</h3>
-        </div>
-        <div className='flex'>
-            <img src={ReportsIcon} alt="reports icon" />
-            <h3>Reports</h3>
-        </div>
-        <div className='flex'>
-            <img src={SettingsIcon} alt="settings icon" />
-            <h3>Settings</h3>
+        <div className="grid gap-0">
+            <div className='flex'>
+                <img className='w-5 h-5' src={dashIcon} alt="dashboard icon" />
+                <h3>Dashboard</h3>
+            </div>
+            <div className='flex'>
+                <img className='w-5 h-5' src={PatientIcon} alt="patient icon" />
+                <h3>Patients</h3>
+            </div>
+            <div className='flex'>
+                <img className='w-5 h-5' src={doctorIcon}alt="doctor icon" />
+                <h3>Doctor</h3>
+            </div>
+            <div className='flex'>
+                <img className='w-5 h-5' src={PharmacyIcon} alt="pharmacy icon" />
+                <h3>Pharmacy</h3>
+            </div>
+            <div className='flex'>
+                <img className='w-5 h-5' src={ReportsIcon} alt="reports icon" />
+                <h3>Reports</h3>
+            </div>
+            <div className='flex'>
+                <img className='w-5 h-5' src={SettingsIcon} alt="settings icon" />
+                <h3>Settings</h3>
+            </div>
         </div>
         <div className='grid absolute bottom-0'>
             <hr className='w-60' />
             <h3>General</h3>
             <div>
-                <img src="" alt="" />
+                <img className='w-5 h-5' src="" alt="" />
                 <h3>Support</h3>
             </div>
         </div>
