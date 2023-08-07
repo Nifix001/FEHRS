5f16a0c426d8b9f386fa44c157a9e68b25785041
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-08-07 17:59:03 +0100

    update

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Nav from '../components/Nav'
+import SideBar from '../components/SideBar'
 
 const Layout = () => {
   return (
     <React.Fragment>
-      
+      <Nav />
+      <SideBar />
     </React.Fragment>
   )
 }
