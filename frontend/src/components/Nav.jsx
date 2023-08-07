d0495d8cb885d24528aa792b28515bcf4b37ce50
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-08-07 18:10:27 +0100

    update

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import logo from '../assets/Frames.png'
+import notify from '../assets/mi_notification.png'
+import direct from '../assets/ic_baseline-log-out.png'
 
 const Nav = () => {
   return (
     <React.Fragment>
         <header className='flex absolute top-0'>
-            <img src={logo} alt="" />
+            <div>
+                <img src={logo} alt="" />
+            </div>
             <div>
                 <input type="text" placeholder='Search' />
             </div>
             <div>
                 <p>Dave Olumuyiwa</p>
                 <p>Admin</p>
+            </div>
+            <div>
+                <NavLink><img src={notify} alt="notification" /></NavLink>
+                <NavLink><img src={direct} alt="direct" /></NavLink>
             </div>
         </header>
     </React.Fragment>
