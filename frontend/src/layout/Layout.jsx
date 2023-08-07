d1634f50b58ec2f3b2a2ef5aa8940ec729d9a10e
MrLad01 <ladayodeji@gmail.com>
2023-08-07 18:33:05 +0100

    update

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Nav from '../components/Nav'
 import SideBar from '../components/SideBar'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
     <React.Fragment>
       <Nav />
       <SideBar />
+      <Outlet />
     </React.Fragment>
   )
 }
