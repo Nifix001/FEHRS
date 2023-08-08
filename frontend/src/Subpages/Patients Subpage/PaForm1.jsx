a2758e0496d38594386b92d9cf11b38362310bb7
MrLad01 <ladayodeji@gmail.com>
2023-08-08 08:49:09 +0100

    update

--- frontend/src/Subpages/Patients Subpage/PaForm1.jsx
+++ frontend/src/Subpages/Patients Subpage/PaForm1.jsx
@@ -1,10 +1,96 @@
 import React from 'react'
+import { Form } from 'react-router-dom'
 
 const PaForm1 = () => {
   return (
-    <div>
-      1
-    </div>
+    <React.Fragment>
+      <Form>
+        <div className="grid grid-cols-3 grid-rows-2">
+          <div className="grid">
+            <label htmlFor="">First Name</label>
+            <input type="text" placeholder='Ayomide' />
+          </div>
+          <div className="grid">
+            <label htmlFor="">Last Name</label>
+            <input type="text" placeholder='Kanayo' />
+          </div>
+          <div className="grid">
+            <label htmlFor="">Other Name</label>
+            <input type="text" placeholder='Festus' />
+          </div>
+          <div className="grid">
+            <label htmlFor="">Matric Number</label>
+            <input type="text" placeholder='___ /__ /_____' />
+          </div>
+          <div className="grid">
+            <label htmlFor="">Date of Birth</label>
+            <input type="text" placeholder='__ /__ /____' />
+          </div>
+          <div className="grid">
+            <label htmlFor="">Phone Number</label>
+            <div className="flex">
+              <div>
+                <img src="" alt="" />
+                <h4>+234</h4>
+                <img src="" alt="" />
+              </div>
+              <input type="text" placeholder='Festus' />
+            </div>
+          </div>
+        </div>
+        <div className="flex">
+          <div className="grid">
+            <label htmlFor="">Gender</label>
+            <div className="flex">
+              <div className="flex">
+                <input type="radio" name="" id="" />
+                <h4>Male</h4>
+              </div>
+              <div className="flex">
+                <input type="radio" name="" id="" />
+                <h4>Female</h4>
+              </div>
+            </div>
+          </div>
+          <div className="grid">
+            <label htmlFor="">Home Address</label>
+            <input type="text" placeholder='Enter Text Here' />
+          </div>
+        </div>
+        <hr />
+        <header>Next of Kin</header>
+        <div className="grid grid-cols-3 grid-rows-2">
+          <div className="grid">
+            <label htmlFor="">First Name</label>
+            <input type="text" placeholder='Ayomide' />
+          </div>
+          <div className="grid">
+            <label htmlFor="">Last Name</label>
+            <input type="text" placeholder='Kanayo' />
+          </div>
+          <div className="grid">
+            <label htmlFor="">Other Name</label>
+            <input type="text" placeholder='Festus' />
+          </div>
+          <div className="grid">
+            <label htmlFor="">Relationship</label>
+            <input type="text" placeholder='' />
+          </div>
+          <div className="grid">
+            <label htmlFor="">Gender</label>
+            <input type="text" placeholder='' />
+          </div>
+          <div className="grid">
+            <label htmlFor="">Date of Birth</label>
+            <input type="text" placeholder='__ / __ / ____' />
+          </div>
+        </div>
+        <div className="flex">
+          <button type='submit'>Save and Continue</button>
+          <button type='submit'>Previous</button>
+        </div>
+      </Form>
+    </React.Fragment>
   )
 }
 
--- frontend/src/Subpages/Patients.jsx
+++ frontend/src/Subpages/Patients.jsx
@@ -4,8 +4,8 @@ import { NavLink, Outlet } from 'react-router-dom'
 const Patients = () => {
   return (
     <React.Fragment>
-     <div className='w-281 h-142.5 bg-white border-l  relative top-20 left-60 -ml-0.5 py-4 px-6'>
-        <header>Add New Patients(Students)</header>
+     <div className='w-281 h-142.5 bg-white border-l  relative top-20 left-60 -ml-0.5 py-4 px-6 patients'>
+        <header>Add New Patient (Students)</header>
         <div className="flex">
           <NavLink to="personal">Personal information</NavLink>
           <NavLink to="info">Medical information</NavLink>
