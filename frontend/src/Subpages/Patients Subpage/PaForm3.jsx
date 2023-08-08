5e04ed684c886fee8d4e8376968bc7a53aabf60a
MrLad01 <ladayodeji@gmail.com>
2023-08-08 09:21:44 +0100

    update

--- frontend/src/Subpages/Patients Subpage/PaForm3.jsx
+++ frontend/src/Subpages/Patients Subpage/PaForm3.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 
 const PaForm3 = () => {
   return (
-    <div>
-      3
-    </div>
+    <React.Fragment>
+      <div className="flex">
+        <div className="grid">
+          <label htmlFor="">Test Typr</label>
+          <input type="text" />
+        </div>
+        <div className="grid">
+          <label htmlFor="">Date of Test</label>
+          <input type="text" placeholder='__ /__ /____' />
+        </div>
+        <div className="grid">
+          <label htmlFor="">Test Location</label>
+          <input type="text" />
+        </div>
+      </div>
+      <div className="grid">
+        <label htmlFor="">Comment</label>
+        <textarea name="" id="" cols="30" rows="10" placeholder='Enter text here' ></textarea>
+      </div>
+    </React.Fragment>
   )
 }
 
