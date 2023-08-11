e63a99cc82928ea98a0dddfc4f30d0294ebbca2d
MrLad01 <ladayodeji@gmail.com>
2023-08-11 06:12:41 +0100

    update

--- frontend/src/Subpages/Pharmacy.jsx
+++ frontend/src/Subpages/Pharmacy.jsx
@@ -15,7 +15,9 @@ const Pharmacy = () => {
         </div>
         <div className='flex gap-4 items-center'>
         <NavLink to="" className='text-primary font-span text-base font-normal py-2 px-5 hover:text-white hover:bg-primary border border-primary rounded-md'>Add New</NavLink>
-        <NavLink to="" className='text-primary font-span text-base font-normal py-2 px-5 hover:text-white hover:bg-primary border border-primary rounded-md'>Import</NavLink>
+        <NavLink to="" className='text-primary font-span text-base font-normal py-2 px-5 hover:text-white hover:bg-primary border border-primary rounded-md'>Import
+          <input type="file" />
+        </NavLink>
         <div className='flex gap-4'>
         <img className='relative left-12' src={search} alt="search" />
         <input type="text"  className='border rounded-md font-span font-normal text-base border-solid text-gray-300 border-gray-300 w-56 h-10 p-2'/>
