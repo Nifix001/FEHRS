258a508b6ef49800b36c8567ba3f264e621ccebc
MrLad01 <ladayodeji@gmail.com>
2023-10-12 14:25:57 +0100

    update

--- frontend/src/Subpages/Dashboard.jsx
+++ frontend/src/Subpages/Dashboard.jsx
@@ -6,6 +6,7 @@ const Dashboard = () => {
   return (
     <React.Fragment>
         <div className = 'w-281 h-142.5 bg-tertiary-4 border-l relative top-20 left-60 -ml-0.5 overflow-x-hidden overflow-y-hidden'>
+          <div className = 'scale-95'>
             <header>
               <p className = 'font-span text-base font-normal  ml-8 mt-2'> Dashboard &gt; Summary </p>
             </header>
@@ -13,6 +14,7 @@ const Dashboard = () => {
             <SummaryCard />
             
             <Graph />
+          </div>
         </div>
     </React.Fragment>
   )
