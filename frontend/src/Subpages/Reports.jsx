c149d1b12e4a04539bba85fd8c4db18e30b8b894
MrLad01 <ladayodeji@gmail.com>
2023-11-06 23:48:51 +0100

    update

--- frontend/src/Subpages/Reports.jsx
+++ frontend/src/Subpages/Reports.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Reports = () => {
   return (
-    <div>
-      
-    </div>
+    <React.Fragment>
+      <div div className = 'w-[80vw] h-160.5 bg-white border-l  relative top-20 left-60 -ml-0.5 py-2 px-3 patients bg-opacity-60 '></div>
+    </React.Fragment>
   )
 }
 
