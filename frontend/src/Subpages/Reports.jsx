bd3aabfc08755cd53b2a042a91092a96abae8ac1
MrLad01 <ladayodeji@gmail.com>
2023-11-07 05:51:11 +0100

    update

--- frontend/src/Subpages/Reports.jsx
+++ frontend/src/Subpages/Reports.jsx
@@ -3,7 +3,19 @@ import React from 'react'
 const Reports = () => {
   return (
     <React.Fragment>
-      <div div className = 'w-[80vw] h-160.5 bg-white border-l  relative top-20 left-60 -ml-0.5 py-2 px-3 patients bg-opacity-60 '></div>
+      <div div className = 'w-fit h-160.5 bg-[#f9f9f9] border-l  relative top-20 left-60 -ml-0.5 py-6 px-14 patients bg-opacity-60 '>
+        <div className = 'flex gap-7 ' >
+          <div className = ' bg-white h-52 w-[36vw] rounded-md grid px-2 py-8 ' >
+            <h1> Overview </h1>
+            <div className="flex justify-around items-center "> </div>
+            <div className="flex justify-around items-center ">  </div>
+          </div>
+          <div className = ' bg-white h-52 w-[36vw] rounded-md ' >
+
+          </div>
+
+        </div>
+      </div>
     </React.Fragment>
   )
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -12,6 +12,7 @@ export default {
     "./src/Subpages/Patients.jsx",
     "./src/Subpages/Dashboard.jsx",
     "./src/Subpages/Pharmacy.jsx",
+    "./src/Subpages/Reports.jsx",
     "./src/Subpages/Patients Subpage/NewPatients.jsx",
     "./src/Subpages/Patients Subpage/PatientsPage.jsx",
     "./src/Subpages/Patients Subpage/PaForm1.jsx",
