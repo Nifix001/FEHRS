624ce2235e2d4aaa30f661da17ab88ebf566831f
MrLad01 <ladayodeji@gmail.com>
2023-11-07 06:44:18 +0100

    update

--- frontend/src/Subpages/Reports.jsx
+++ frontend/src/Subpages/Reports.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Reports = () => {
   return (
     <React.Fragment>
-      <div div className = 'w-fit h-160.5 bg-[#f9f9f9] border-l  relative top-20 left-60 -ml-0.5 py-6 px-14 patients bg-opacity-60 '>
+      <div div className = 'w-fit h-160.5 bg-[#f9f9f9] border-l  relative top-20 left-60 -ml-0.5 py-6 px-14 patients bg-opacity-60 grid '>
         <div className = 'flex gap-7 ' >
           <div className = ' bg-white h-52 w-[36vw] rounded-md grid px-2 py-8 ' >
             <h1> Overview </h1>
@@ -74,8 +74,8 @@ const Reports = () => {
               </tbody>
             </table>
           </div>
-
         </div>
+        <div className = 'bg-white h-52 w-[74vw] rounded-md ' ></div>
       </div>
     </React.Fragment>
   )
