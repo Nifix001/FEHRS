f8e77b7e923c0e24468269f2335365858146958c
MrLad01 <ladayodeji@gmail.com>
2023-11-19 13:36:31 +0100

    update

--- frontend/src/helpers/helpers.js
+++ frontend/src/helpers/helpers.js
@@ -1,28 +1,20 @@
-import { useEffect, useState } from "react";
 import axios from "axios";
-import useLocalStorage from "../hooks/useLocalStorage";
-import { useUser } from "../context/UserContext";
-import { redirect, useLocation } from "react-router-dom";
 
-// axios.defaults.baseURL = 'http://127.0.0.1:8000'
+
 axios.defaults.withCredentials = true;
 
 export default async function processLogin(email, password, setLoading, setToken, e, history, user, setUser) {
     e.preventDefault();
-    // const { user } = useUser();
-
 
     try {
         const response = await axios.get('http://localhost:8000/sanctum/csrf-cookie');
-        // console.log(response);      
-            if (response) {
+
+        if (response) {
                 // Get the CSRF token from the cookies
-                // console.log(document.cookie);
                 const csrfToken = document.cookie.split('; ')
                                     .find(row => row.startsWith('XSRF-TOKEN='))
                                     .split('=')[1];
-                // console.log('CSRF Token:', csrfToken);
-  
+
             // Set the CSRF token in state or wherever you need it
             setToken(csrfToken);
             
@@ -35,12 +27,10 @@ export default async function processLogin(email, password, setLoading, setToken
                 headers: {
                     Accept: 'application/json',
                     'X-XSRF-TOKEN': decodeURIComponent(csrfToken),
-                    // origin: 'localhost:8000'
                 }
             }
             );
             console.log(loginResponse);
-            // localStorage.setItem('user', JSON.stringify(loginResponse.data));
             !user && setUser(loginResponse.data);
             history('/user');
         } else {
