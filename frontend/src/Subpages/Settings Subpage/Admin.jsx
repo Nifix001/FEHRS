49bdb31c9c46c7d4da254e2f4da872fed7cbf90f
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-12-02 22:53:05 +0100

    update

--- frontend/src/Subpages/Settings Subpage/Admin.jsx
+++ frontend/src/Subpages/Settings Subpage/Admin.jsx
@@ -3,8 +3,41 @@ import React from 'react'
 const Admin = () => {
   return (
     <div className = '  h-[83.2vh] w-[60vw] flex flex-col gap-4 ' >
-        <div className = ' w-full h-12 rounded-md bg-white ' ></div>
-        <div className = ' w-full h-full rounded-xl bg-white ' ></div>
+        <div className = ' w-full h-12 rounded-md bg-white ' >
+          <h3>Admin Mangement</h3>
+        </div>
+        <div className = ' w-full h-full rounded-xl bg-white ' >
+          <form action="">
+            <h1>Assign a Role</h1>
+            <div className='flex flex-col'>
+              <div className='flex'>
+                <input type="radio" name="" id="" />
+                <div className='flex flex-col'>
+                  <label htmlFor="">Doctor</label>
+                  <p>Assign as Doctor</p>
+                </div>
+              </div>
+              <div className='flex'>
+                <input type="radio" name="" id="" />
+                <div className='flex flex-col'>
+                  <label htmlFor="">Phamarcist</label>
+                  <p>Assign as Phamarcist</p>
+                </div>
+              </div>
+              <div className='flex'>
+                <input type="radio" name="" id="" />
+                <div className='flex flex-col'>
+                  <label htmlFor="">Chief Host</label>
+                  <p>Assign as Chief Host</p>
+                </div>
+              </div>
+            </div>
+            <div className='flex'>
+              <button>Assign Role</button>
+              <button>Cancel</button>
+            </div>
+          </form>
+        </div>
     </div>
   )
 }
--- frontend/src/Subpages/Settings Subpage/Password.jsx
+++ frontend/src/Subpages/Settings Subpage/Password.jsx
@@ -3,8 +3,22 @@ import React from 'react'
 const Password = () => {
   return (
     <div className = '  h-[83.2vh] w-[60vw] flex flex-col gap-4 ' >
-        <div className = ' w-full h-12 rounded-md bg-white ' ></div>
-        <div className = ' w-full h-full rounded-xl bg-white ' ></div>
+        <div className = ' w-full h-12 rounded-md bg-white ' >
+          <h3>Change Password</h3>
+        </div>
+        <div className = ' w-full h-full rounded-xl bg-white ' >
+          <form action="">
+            <div className='flex flex-col'>
+            <input type="password" name="" id="" placeholder='Enter Old Password'/>
+            <input type="password" name="" id="" placeholder='Enter New Password'/>
+            <input type="password" name="" id="" placeholder='Enter New Password'/>
+            </div>
+            <div className='flex'>
+              <button>Change Password</button>
+              <button>Cancel</button>
+            </div>
+          </form>
+        </div>
     </div>
   )
 }
