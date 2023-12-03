16ff04a9237ba3074a91c1daca7ac61321b116f8
MrLad01 <ladayodeji@gmail.com>
2023-12-03 21:10:29 +0100

    update

--- frontend/src/Subpages/Settings Subpage/Admin.jsx
+++ frontend/src/Subpages/Settings Subpage/Admin.jsx
@@ -1,40 +1,51 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Admin = () => {
+
+  const location = useNavigate();
+
   return (
     <div className = '  h-[83.2vh] w-[60vw] flex flex-col gap-4 ' >
-        <div className = ' w-full h-12 rounded-md bg-white ' >
-          <h3>Admin Mangement</h3>
+        <div className = ' w-full h-12 rounded-md bg-white flex items-center p-3 cursor-pointer '
+              onClick = {
+                (e) => {
+                  e.preventDefault();
+                  location('/user/settings')
+                }
+              }
+        >
+          <h3 className = 'text-xs' >Admin Mangement</h3>
         </div>
-        <div className = ' w-full h-full rounded-xl bg-white ' >
+        <div className = ' w-full h-full rounded-xl bg-white p-9 ' >
           <form action="">
-            <h1>Assign a Role</h1>
-            <div className='flex flex-col'>
-              <div className='flex'>
-                <input type="radio" name="" id="" />
+            <h1 className = 'text-2xl my-10' >Assign a Role</h1>
+            <div className='flex flex-col gap-10 mb-24 '>
+              <div className='flex items-center gap-6 '>
+                <input type="radio" name="role" id="" className = ' w-7 h-7 ' />
                 <div className='flex flex-col'>
-                  <label htmlFor="">Doctor</label>
-                  <p>Assign as Doctor</p>
+                  <label htmlFor="" className = 'font-bold text-base' >Doctor</label>
+                  <p className = 'text-sm text-gray-400' >Assign as Doctor</p>
                 </div>
               </div>
-              <div className='flex'>
-                <input type="radio" name="" id="" />
+              <div className='flex items-center gap-6 '>
+                <input type="radio" name="role" id="" className = ' w-7 h-7 ' />
                 <div className='flex flex-col'>
-                  <label htmlFor="">Phamarcist</label>
-                  <p>Assign as Phamarcist</p>
+                  <label htmlFor="" className = 'font-bold text-base' >Phamarcist</label>
+                  <p className = 'text-sm text-gray-400' >Assign as Phamarcist</p>
                 </div>
               </div>
-              <div className='flex'>
-                <input type="radio" name="" id="" />
+              <div className='flex items-center gap-6 '>
+                <input type="radio" name="role" id="" className = ' w-7 h-7 ' />
                 <div className='flex flex-col'>
-                  <label htmlFor="">Chief Host</label>
-                  <p>Assign as Chief Host</p>
+                  <label htmlFor="" className = 'font-bold text-base' >Chief Host</label>
+                  <p className = 'text-sm text-gray-400' >Assign as Chief Host</p>
                 </div>
               </div>
             </div>
-            <div className='flex'>
-              <button>Assign Role</button>
-              <button>Cancel</button>
+            <div className='flex justify-center gap-4'>
+              <button className = 'border px-6 py-2 rounded-md bg-primary text-white '>Assign Role</button>
+              <button className = 'border px-6 py-2 rounded-md border-primary text-primary '>Cancel</button>
             </div>
           </form>
         </div>
--- frontend/src/Subpages/Settings Subpage/Password.jsx
+++ frontend/src/Subpages/Settings Subpage/Password.jsx
@@ -1,21 +1,41 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Password = () => {
+  
+  const location = useNavigate();
+
   return (
     <div className = '  h-[83.2vh] w-[60vw] flex flex-col gap-4 ' >
-        <div className = ' w-full h-12 rounded-md bg-white ' >
-          <h3>Change Password</h3>
+        <div className = ' w-full h-12 rounded-md bg-white flex items-center p-3 cursor-pointer'
+          onClick = {
+            (e) => {
+              e.preventDefault();
+              location('/user/settings')
+            }
+          }
+        >
+          <h3 className = 'text-xs' >Change Password</h3>
         </div>
-        <div className = ' w-full h-full rounded-xl bg-white ' >
+        <div className = ' w-full h-full rounded-xl bg-white p-9 ' >
           <form action="">
-            <div className='flex flex-col'>
-            <input type="password" name="" id="" placeholder='Enter Old Password'/>
-            <input type="password" name="" id="" placeholder='Enter New Password'/>
-            <input type="password" name="" id="" placeholder='Enter New Password'/>
+            <div className='flex flex-col mb-28 gap-3 '>
+              <div className = 'grid' >
+                <label htmlFor="" className = ' bg-white p-2 text-xs relative top-4 left-2 w-fit '> Enter Old Password </label>
+                <input type="password" name="" id="" placeholder='Enter Old Password' className = 'border outline-none block py-4 pl-4 rounded-lg pr-4 w-94 text-lg ' />
+              </div>
+              <div className = 'grid' >
+                <label htmlFor="" className = ' bg-white p-2 text-xs relative top-4 left-2 w-fit '> Enter New Password </label>
+                <input type="password" name="" id="" placeholder='Enter New Password' className = 'border outline-none block py-4 pl-4 rounded-lg pr-4 w-94 text-lg ' />
+              </div>
+              <div className = 'grid' >
+                <label htmlFor="" className = ' bg-white p-2 text-xs relative top-4 left-2 w-fit '> Enter New Password </label>
+                <input type="password" name="" id="" placeholder='Enter New Password' className = 'border outline-none block py-4 pl-4 rounded-lg pr-4 w-94 text-lg ' />
+              </div>
             </div>
-            <div className='flex'>
-              <button>Change Password</button>
-              <button>Cancel</button>
+            <div className='flex justify-center gap-4'>
+              <button className = 'border px-6 py-2 rounded-md bg-primary text-white '>Change Password</button>
+              <button className = 'border px-6 py-2 rounded-md border-primary text-primary '>Cancel</button>
             </div>
           </form>
         </div>
