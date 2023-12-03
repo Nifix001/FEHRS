8481302deeb6547ed2577e28201772cce79ce628
MrLad01 <ladayodeji@gmail.com>
2023-12-03 20:32:13 +0100

    update

--- frontend/src/Subpages/Settings Subpage/Personal.jsx
+++ frontend/src/Subpages/Settings Subpage/Personal.jsx
@@ -2,44 +2,44 @@ import React from 'react'
 
 const Personal = () => {
   return (
-    <div className = '  h-[83.2vh] w-[60vw] bg-white rounded-xl '>
+    <div className = '  h-[83.2vh] w-[60vw] bg-white rounded-xl p-9 '>
       <form action="">
-        <h1> Personal Info </h1>
-        <div className = ' flex gap-4 items-center ' >
-             <div className = 'w-20 h-20 rounded-full bg-gray-400' ></div>
+        <h1 className = ' font-bold mb-6 '> Personal Info </h1>
+        <div className = ' flex gap-6 items-center mb-2 ' >
+             <div className = 'w-20 h-20 rounded-full bg-gray-200' ></div>
              <div className = "flex flex-col gap-2 items-start ">
-                <button className = ' bg-slate-400 text-white ' > Upload photo  </button>
-                <button> Remove </button>
+                <button className = ' bg-primary text-white px-3 py-[5px] text-sm rounded-md ' > Upload photo  </button>
+                <button className = 'text-sm text-red-400 px-4' > Remove </button>
              </div>
         </div>
-        <div>
-          <label htmlFor = ""> Portfolio </label>
-          <button> Doctor </button>
+        <div className = 'mb-4' >
+          <label htmlFor = "" className = ' bg-white p-2 text-xs relative top-3 left-2 ' > Portfolio </label>
+          <button className = 'block border py-3 pl-3 rounded-lg text-sm pr-56' > Doctor </button>
         </div>
         <div className="grid grid-cols-2 grid-rows-3 ">
           <div className = 'flex flex-col' >
-            <label htmlFor = ""> First Name </label>
-            <input type="text" placeholder = 'Kanayo'  />
+            <label htmlFor = "" className = ' bg-white p-2 text-xs relative top-4 left-2 w-fit '> First Name </label>
+            <input type="text" placeholder = 'Kanayo' className = 'border outline-none block py-3 pl-3 rounded-lg text-sm pr-4 w-72 '  />
           </div>
           <div className = 'flex flex-col ' >
-             <label htmlFor = ""> Last Name </label>
-            <input type="text" placeholder = 'Olumuyiwa'  />
+             <label htmlFor = "" className = ' bg-white p-2 text-xs relative top-4 left-2 w-fit '> Last Name </label>
+            <input type="text" placeholder = 'Olumuyiwa' className = 'border outline-none block py-3 pl-3 rounded-lg text-sm pr-4 w-72 '  />
           </div>
           <div className = 'flex flex-col ' >
-             <label htmlFor = ""> Gender </label>
-            <input type="text" placeholder = 'Male'  />
+             <label htmlFor = "" className = ' bg-white p-2 text-xs relative top-4 left-2 w-fit '> Gender </label>
+            <input type="text" placeholder = 'Male' className = 'border outline-none block py-3 pl-3 rounded-lg text-sm pr-4 w-72 '  />
           </div>
           <div className = 'flex flex-col ' >
-             <label htmlFor = ""> Date of Birth </label>
-            <input type="text" placeholder = '30/12/1993'  />
+             <label htmlFor = "" className = ' bg-white p-2 text-xs relative top-4 left-2 w-fit '> Date of Birth </label>
+            <input type="date" placeholder = '30/12/1993' className = 'border outline-none block py-3 pl-3 rounded-lg text-sm pr-4 w-72 '  />
           </div>
           <div className = 'flex flex-col ' >
-             <label htmlFor = ""> Phone Number </label>
-            <input type="text" placeholder = 'Olumuyiwa'  />
+             <label htmlFor = "" className = ' bg-white p-2 text-xs relative top-4 left-2 w-fit '> Phone Number </label>
+            <input type="tel" placeholder = 'Olumuyiwa' className = 'border outline-none block py-3 pl-3 rounded-lg text-sm pr-4 w-72 '  />
           </div>
           <div className = 'flex flex-col ' >
-             <label htmlFor = ""> State </label>
-            <input type="text" placeholder = 'Lagos'  />
+             <label htmlFor = "" className = ' bg-white p-2 text-xs relative top-4 left-2 w-fit '> State </label>
+            <input type="text" placeholder = 'Lagos' className = 'border outline-none block py-3 pl-3 rounded-lg text-sm pr-4 w-72 '  />
           </div>
         </div>
         <button> Update Profile </button>
--- frontend/src/Subpages/Settings.jsx
+++ frontend/src/Subpages/Settings.jsx
@@ -4,7 +4,7 @@ import { NavLink, Outlet } from 'react-router-dom'
 const Settings = () => {
   return (
     <div className = 'w-[1111px] h-fit bg-[#f9f9f9] border-l  relative top-20 left-60 -ml-0.5 pt-4 pb-8 px-14 settings bg-opacity-60 flex gap-8 '>
-      <div className = ' h-[83.2vh] w-64 bg-white flex flex-col p-3 gap-20 rounded-xl ' >
+      <div className = ' h-[83.2vh] w-64 bg-white flex flex-col p-3 gap-12 rounded-xl ' >
         <h1 className = ' font-bold text-[18px] py-2 px-1 ' > Settings </h1>
         <div className = "flex flex-col ">
             <NavLink
