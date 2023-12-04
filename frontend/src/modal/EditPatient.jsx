2e26ee22a6476986ba5292496884dcdc077cf6f5
MrLad01 <ladayodeji@gmail.com>
2023-12-04 21:37:13 +0100

    update

--- frontend/src/modal/EditPatient.jsx
+++ frontend/src/modal/EditPatient.jsx
@@ -1,22 +1,70 @@
 // EditModal.js
 
-import React from 'react';
+import React, { useState } from 'react';
 
 const EditPatient = ({ isOpen, onClose, patient }) => {
   if (!isOpen) return null;
 
+  const [ firstname, setFirstname ] = useState(patient.firstname);
+  const [ lastname, setLastname ] = useState(patient.lastname);  
+  const [ matric_no, setMatric_no ] = useState(patient.matric_no);
+  const [ phone_no, setPhone_no ] = useState(patient.phone_no);
+  const [ email, setEmail ] = useState(patient.email);
+
   return (
     <div className="fixed inset-0 z-50 overflow-auto bg-black bg-opacity-50 flex">
-      <div className="relative p-8 bg-white w-96 mx-auto my-12 rounded-md shadow-lg">
+      <div className="relative p-8 bg-white w-fit mx-auto my-12 rounded-md shadow-lg">
         <button
-          className="absolute top-0 right-0 p-2 cursor-pointer"
+          className="absolute top-8 right-5 p-2 cursor-pointer text-xs text-red-400"
           onClick={onClose}
         >
           Close
         </button>
         <h2 className="text-2xl font-semibold mb-4">Edit Patient</h2>
-        {/* Add your edit form components here */}
-        {/* You can use the patient prop to pre-fill the form */}
+        <div className="block">
+            <div className="flex gap-4">
+                <div className="flex flex-col ">
+                    <label htmlFor="" className = ' bg-white p-2 text-xs relative top-4 left-2 w-fit '> First Name </label>
+                    <input type="text" value = { firstname } className = 'border outline-none block py-3 pl-3 rounded-lg text-sm pr-4 w-72 ' onChange = { (e) => {
+                                                                                    e.preventDefault();        
+                                                                                    setFirstname(e.target.value)
+                                                                                } }  />
+                </div>
+                <div className="flex flex-col ">
+                    <label htmlFor="" className = ' bg-white p-2 text-xs relative top-4 left-2 w-fit '> Last Name </label>
+                    <input type="text" value = { lastname } className = 'border outline-none block py-3 pl-3 rounded-lg text-sm pr-4 w-72 ' onChange = { (e) => {
+                                                                                    e.preventDefault();        
+                                                                                    setLastname(e.target.value)
+                                                                                } }  />
+                </div>
+            </div>
+            <div className="flex gap-4">
+                <div className="flex flex-col ">
+                        <label htmlFor="" className = ' bg-white p-2 text-xs relative top-4 left-2 w-fit '> Matric Number </label>
+                        <input type="text" value = { matric_no } className = 'border outline-none block py-3 pl-3 rounded-lg text-sm pr-4 w-72 ' onChange = { (e) => {
+                                                                                        e.preventDefault();        
+                                                                                        setMatric_no(e.target.value)
+                                                                                    } }  />
+                </div>
+                <div className="flex flex-col ">
+                        <label htmlFor="" className = ' bg-white p-2 text-xs relative top-4 left-2 w-fit '> Phone Number </label>
+                        <input type="number" value = { phone_no } className = 'border outline-none block py-3 pl-3 rounded-lg text-sm pr-4 w-72 ' onChange = { (e) => {
+                                                                                        e.preventDefault();        
+                                                                                        setPhone_no(e.target.value)
+                                                                                    } }  />
+                </div>
+            </div>
+            <div className="flex flex-col ">
+                <label htmlFor="" className = ' bg-white p-2 text-xs relative top-4 left-2 w-fit '> Email Address </label>
+                <input type="text" value = { email } className = 'border outline-none block py-3 pl-3 rounded-lg text-sm pr-4 w-72 ' onChange = { (e) => {
+                                                                                        e.preventDefault();        
+                                                                                        setEmail(e.target.value)
+                                                                                    } }  />
+            </div>
+            <div className = "flex w-full items-center justify-center mt-20">
+               <button className = ' bg-primary text-white px-4 py-[6px] text-sm  rounded-md'  > Update </button>                                                                     
+            </div>
+        </div>
       </div>
     </div>
   );
