cc59f953640ddcbc7879f4ec26cd85f9cdb7b8c6
MrLad01 <ladayodeji@gmail.com>
2023-12-04 06:18:19 +0100

    update

--- frontend/src/Subpages/Doctor.jsx
+++ frontend/src/Subpages/Doctor.jsx
@@ -142,7 +142,7 @@ const Doctor = () => {
             <SearchPatient patients = { searchPatient }  />
         </div>
          : 
-         <div className = 'bg-white w-[1014px] h-[510px] p-2 absolute z-10 mt-6 rounded-xl shadow-sm flex items-center justify-center text-5xl opacity-50'>
+         <div className = 'bg-white w-[1014px] h-[510px] p-2 absolute z-10 mt-6 rounded-xl shadow-sm flex items-center justify-center text-5xl opacity-70'>
             <h1 className = ' opacity-5 text-center ' > Please type in the search box above and hit the Search button </h1>
         </div> }
 
