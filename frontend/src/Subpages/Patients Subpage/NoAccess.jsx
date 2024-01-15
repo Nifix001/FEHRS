c6471dded4c32a7a28046e895febb68834fe3e34
MrLad01 <ladayodeji@gmail.com>
2024-01-15 04:57:44 +0100

    update

--- frontend/src/Subpages/Patients Subpage/NoAccess.jsx
+++ frontend/src/Subpages/Patients Subpage/NoAccess.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const NoAccess = () => {
   return (
     <>
-      
+      <div className='w-full h-full flex items-center justify-center' >
+            <div className='grid' >
+                
+            </div>
+      </div>
     </>
   )
 }
