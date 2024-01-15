a4db303c4718bb7f4fe3053f7445a5fa093479e3
MrLad01 <ladayodeji@gmail.com>
2024-01-15 05:22:55 +0100

    update

--- frontend/src/Subpages/Patients.jsx
+++ frontend/src/Subpages/Patients.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import NoAccess from './NoAccess'
 import { useUser } from '../context/UserContext'
+import NoAccess from './NoAccess';
 
 const Patients = () => {
 
@@ -10,14 +10,16 @@ const Patients = () => {
   const userAllowed = user.role_id === 1 || user.role_id === 4;
 
   return (  
-  <React.Fragment>
-    { userAllowed  ? 
-        <div className = 'w-[80vw] h-160.5 bg-white border-l  relative top-20 left-60 -ml-0.5 py-2 px-3 patients bg-opacity-60 '>
-          <Outlet /> 
-        </div>  
-    : <NoAccess />
-     }
-  </React.Fragment>
+    <React.Fragment>
+      { userAllowed  ? 
+          <div className = 'w-[80vw] h-160.5 bg-white border-l  relative top-20 left-60 -ml-0.5 py-2 px-3 patients bg-opacity-60 '>
+                <Outlet /> 
+          </div>  
+          : 
+          // <NoAccess />
+          <></>
+        }
+    </React.Fragment>
   )
 }
 
