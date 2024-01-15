a6845bcdf9ea05d38ce383a0faec555760fea49f
MrLad01 <ladayodeji@gmail.com>
2024-01-15 05:01:04 +0100

    update

--- frontend/src/Subpages/Patients Subpage/NoAccess.jsx
+++ frontend/src/Subpages/Patients Subpage/NoAccess.jsx
@@ -5,7 +5,10 @@ const NoAccess = () => {
     <>
       <div className='w-full h-full flex items-center justify-center' >
             <div className='grid' >
-                
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}                   stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z" />
+            </svg>
+            <p> You do not have access to this page. Please contact the admin for more information. </p>
             </div>
       </div>
     </>
