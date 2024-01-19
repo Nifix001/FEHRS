db36e47ff0d07465192e2218b146895c688986a5
MrLad01 <ladayodeji@gmail.com>
2024-01-19 21:53:07 +0100

    update

--- frontend/src/modal/PrescribeDrug.jsx
+++ frontend/src/modal/PrescribeDrug.jsx
@@ -27,7 +27,7 @@ const PrescribeDrug = ( { onClose } ) => {
 
   return (
     <div className="fixed inset-0 z-50 overflow-auto bg-black bg-opacity-50 flex items-center justify-center">
-        <div className="relative p-8 bg-white w-fit mx-auto my-12 rounded-2xl shadow-lg">
+        <div className="relative p-8 bg-white w-fit mx-auto my-12 rounded-2xl shadow-lg px-16">
           {/* <button
             className="absolute top-8 right-5 p-2 cursor-pointer text-xs text-red-400"
             onClick={onClose}
@@ -45,19 +45,23 @@ const PrescribeDrug = ( { onClose } ) => {
               {/* <p>Are you sure you want to prescribe the following drugs?</p> */}
               <div>
                 {inputSets.map(inputSet => (
-                  <div key={inputSet.id} className="input-group flex gap-4">
-                    <label htmlFor=""> Drug Quantity </label>
-                    <input type="text"
-                    className='border'  />
-                    <label htmlFor={`input${inputSet.id}`}>{`Input ${inputSet.id}:`}</label>
-                    <input
-                      type="text"
-                      className='border'
-                      id={`input${inputSet.id}`}
-                      name={`input${inputSet.id}`}
-                      value={inputSet.value}
-                      onChange={e => handleInputChange(inputSet.id, e.target.value)}
-                    />
+                  <div key={inputSet.id} className="input-group flex gap-6">
+                    <div className="flex flex-col relative">
+                      <label htmlFor="" className='text-xs' > Drug </label>
+                      <input type="text"
+                      className='border p-2'  />
+                    </div>
+                    <div className="flex flex-col relative">
+                      <label htmlFor={`input${inputSet.id}`} className='text-xs' >{`Quantity ${inputSet.id}:`}</label>
+                      <input
+                        type="text"
+                        className='border p-2'
+                        id={`input${inputSet.id}`}
+                        name={`input${inputSet.id}`}
+                        value={inputSet.value}
+                        onChange={e => handleInputChange(inputSet.id, e.target.value)}
+                      />
+                    </div>
                   </div>
                 ))}
                 <button type="button" onClick={handleAddInputSet}>
